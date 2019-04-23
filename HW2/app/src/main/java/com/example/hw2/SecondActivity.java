cad8a5961aeb8a52c437c003bdd523dc7cff5522
YunFSui <syatwork@outlook.com>
2019-04-22 21:50:20 -0700

    update to add lifecycles

--- HW2/app/src/main/java/com/example/hw2/SecondActivity.java
+++ HW2/app/src/main/java/com/example/hw2/SecondActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 public class SecondActivity  extends AppCompatActivity  {
     private static  final String TAG = "Message from 1st Act: ";
+    private static  final String CON = "Message from 2nd Act: ";
 
     @Override
     protected void onCreate(Bundle savedState) {
@@ -21,11 +22,45 @@ public class SecondActivity  extends AppCompatActivity  {
         TextView label = (TextView)findViewById(R.id.intent_message);
         label.setText(message);
 
-        Log.i(TAG, this.getLifecycle()
-                .getCurrentState()
-                .toString());
+        if (message != null){
+            Log.i(TAG, message);
+            Log.i(CON, this.getLifecycle()
+                    .getCurrentState()
+                    .toString());
+        } else {
+            Log.i(CON, "There is no message.");
+        }
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(CON, "started");
+        Log.i(CON, this.getLifecycle().getCurrentState().toString());
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(CON, "I am back!");
+        Log.i(CON, this.getLifecycle().getCurrentState().toString());
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(CON, "Let's take a break.");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(CON, "Halt your horse!");
+    }
+
+    @Override
+    protected  void onDestroy() {
+        super.onDestroy();
+        Log.i(CON, "I will be back (Thumb up)");
+    }
 }
